4bc7cf2a53e8a8663b9e02e2199a7de0d67ccc71
kariukidan <kariukidan32@gmail.com>
2023-07-13 13:10:10 -0700

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
+int main(void) 
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, sizeof(message) - 1);
 	return (1);
 }
