19a752642ec710056f19a2a6e73508dd7d8aafb3
kariukidan <kariukidan32@gmail.com>
2023-07-18 13:16:57 -0700

    isalpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character returns 1 if true and 0 if otherwise
+ * _islower - checks for lowercase character
  * @c: The character to be checked
  *
  * Return : 1 if c is lowercase, 0 otherwise
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: The character to be checked
+ *
+ * Return : 1 if c is alphabetic character, 0 if otherwise
+ */
+int _isalpha(int c)
+{
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        }
+	else if (c >= 97 && c <= 122)
+	{
+		return (1);	
+	else
+	{
+                return (0);
+        }
+}
