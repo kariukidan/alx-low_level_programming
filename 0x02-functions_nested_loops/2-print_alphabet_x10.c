645e37efe29c5d945e7beeac2517cfc4ad894a40
kariukidan <kariukidan32@gmail.com>
2023-07-18 10:44:56 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ *
+ * Return : Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int i; j;
+	for (j = 0; j <= 10; j++);
+	for (i = 97; i <= 122; i++);
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
