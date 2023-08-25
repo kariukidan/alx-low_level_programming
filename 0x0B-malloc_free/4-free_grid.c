8e1be297b731648081913d8e4273eab4b54444d9
kariukidan <kariukidan32@gmail.com>
2023-08-24 23:50:51 -0700

    Malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,8 +2,7 @@
 #include "main.h"
 
 /**
- * free_grid - frees the memory allocate for the grid
- * created by alloc_grid()
+ * free_grid - frees a 2 dimensional grid 
  * @grid: grid to free
  * @height: height of the grid
  */
