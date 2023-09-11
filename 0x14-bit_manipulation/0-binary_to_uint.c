e39799fc94f40e77a8fa7d13fc9ce236380e0f76
kariukidan <136015791+kariukidan@users.noreply.github.com>
2023-09-11 21:11:05 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,16 +10,18 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int cn, base;
 	int len;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
+	cn = 0;
+	
 	for (len = 0; b[len]; len++)
 	{
 		if (b[len] != '0' && b[len] != '1')
 			return (0);
 	}
 
-	for (base = 1, cn = 0, len--; len >= 0; len--, base *= 2)
+	for (base = 1, len--; len >= 0; len--, base *= 2)
 	{
 		if (b[len] == '1')
 			cn += base;
