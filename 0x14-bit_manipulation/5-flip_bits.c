f2de4b36dd3d7c8dc1162a9032dcb3dee0f1cf2f
kariukidan <136015791+kariukidan@users.noreply.github.com>
2023-11-23 19:24:38 +0300

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * flip_bits - flip bits to convert one number to another number
+ * flip_bits - flips bits to convert one number to another number
  * @n: first number
  * @m: second number to convert to
- * Return: number of bits that was needed to flip
+ * Return: number of bits needed to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
